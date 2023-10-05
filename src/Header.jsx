685a0a6ecc628c17131bd02250641f0e212c2196
Uwancha <sito2alemu@gmail.com>
2023-10-05 21:44:52 +0300

    Add styles, layout and content to header component

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import cartIcon from "./assets/cart_3625090.png"
+import styles from "./styles/header.module.css"
+
 
 function Header() {
     return (
-        <nav>
-            <Link to= "/">Home</Link>
-            <Link to="shop">Shop</Link>
-            <Link to="cart">Cart</Link>
+        <nav className={styles.nav}>
+            <div className={styles.links}>
+                <Link to= "/" className={styles.link}>Home</Link>
+                <Link to="/shop" className={styles.link}>Shop</Link>
+                <Link to="/about" className={styles.link}>About</Link>
+            </div>
+            <div>
+                <Link>
+                <img src={cartIcon} className={styles.cartIcon} alt="shopping cart image by klimir art" />
+                </Link>
+            </div>
         </nav>
     )
 }
