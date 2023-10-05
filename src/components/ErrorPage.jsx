54a610fd77dfde6dba5eb58e8023a27817fd3abc
Uwancha <sito2alemu@gmail.com>
2023-10-05 22:19:29 +0300

    Import and add class name properties to elements

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Link, useRouteError } from "react-router-dom";
+import styles from "../styles/error.module.css"
 
 function ErrorPage() {
     const error = useRouteError()
 
     return (
         <>
-            <div>
-                <h6>Opps! an error Occured</h6>
-                <p>{error.statusText || error.message}</p>
+            <div className={styles.err}>
+                <h4>Opps! an error has Occured</h4>
+                <p className={styles.message}>{error.statusText || error.message}</p>
                 <Link to="/">Back to home page</Link>
             </div>
         </>
