7f951a20b28d6dd482110989efa209cd0486a4f6
Uwancha <sito2alemu@gmail.com>
2023-10-06 09:38:36 +0300

    feat: Add some messages and image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import cartIcon from "../assets/carts_891462.png"
 
 function Home() {
     return (
-        <>
-         <div>
-         
-         </div>
-        </>
+        <div>
+            <div>
+                <div>
+                    <h4>Welcome! Happy Shopping</h4>
+                    <p>You know you can shop anything here!</p>
+                    <p>Well, actually no. This is a demo!</p>
+                </div>
+                <div>
+                    <h5>50$ credit when you first register</h5>
+                    <button>Sign Up</button>
+                </div>
+            </div>
+            <div>
+                <img src={cartIcon} alt="shopping cart. Icon by Pixel Perfect." />
+            </div>
+        </div>
     )
 }
 
