9eee25ec05ebc25bce45a4767c4dc67cb84eeea0
Uwancha <sito2alemu@gmail.com>
2023-10-07 21:09:06 +0300

    Add add to cart button

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -10,6 +10,7 @@ function Product({product}) {
                 <h2>Price: ${product.price}</h2>
                 <button className={styles.button}>Details</button>
             </div>
+            <button className={styles.addToCart}>Add to cart</button>
         </li>
     )
 }
