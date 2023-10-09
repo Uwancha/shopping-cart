a27e762b141f1d5c2e7cb5467ffb50c19297290c
Uwancha <sito2alemu@gmail.com>
2023-10-09 11:01:11 +0300

    Remove unused css module property

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -4,7 +4,7 @@ import styles from "../styles/loading.module.css"
 function Loading() {
     return (
         <div className={styles.loading}>
-            <div className={styles.spin}></div>
+            
         </div>
     )
 }
