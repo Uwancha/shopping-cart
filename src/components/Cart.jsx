8a9c4fbe1199e4d4df5a9a27f1aae4b01ff53de3
Uwancha <sito2alemu@gmail.com>
2023-10-10 22:25:03 +0300

    Improve the cart items page UI/UX Add layout Add contents

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from "react";
 import { CartContext } from "../CartContext";
+import styles from "../styles/cart.module.css"
+import { Link } from "react-router-dom";
 
 function Cart() {
     const {cart} = useContext(CartContext)
@@ -9,21 +11,32 @@ function Cart() {
     }, 0);
 
     return (
-        <div>
+        <div className={styles.cartItems}>
             {cart.length > 0 && (
             <>
-                <h3>Added Items</h3>
+                <h3 className={styles.pageTitle}>Added Items</h3>
        
                 {cart.map(item => (
-                    <div key={item.id}>
-                        <p>{item.title}</p>
-                        <p>Quantity: {item.quantity}</p>
-                    </div>
+                    <Link to={`/shop/${item.id}`} className={styles.link}>
+                        <div key={item.id} className={styles.cart}>
+                            <div className={styles.imgContainer}>
+                                <img src={item.image} alt={item.title}  className={styles.img}/>
+                            </div>
+                            <div className={styles.titlePrice}>
+                                <p>{item.title}</p>
+                                <p>Subtotal: {item.price * item.quantity}</p>
+                                <p>Quantity: {item.quantity}</p>
+                            </div>
+                        
+                            <button className={styles.removebtn} >Remove</button>
+                        
+                        </div>
+                    </Link>
                     )) }
 
-                <p>Total: ${totalPrice}</p>
+                <p className={styles.total}>Total: ${totalPrice}</p>
 
-                <button>Checkout</button>
+                <button className={styles.checkoutbtn}>Checkout</button>
             </>
             ) }
 
