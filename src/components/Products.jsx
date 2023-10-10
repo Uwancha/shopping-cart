121a5d2b37225b3aa800d1b66ae02443099e4fbc
Uwancha <sito2alemu@gmail.com>
2023-10-10 23:06:59 +0300

    refac: Remove redundant memoization

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,18 +1,14 @@
-import React, { useMemo } from "react";
+import React from "react";
 import Product from "./Product";
 import styles from '../styles/products.module.css'
 import Loading from "./Loading";
 
 function Products({products}) {
 
-    const memoizedProducts = useMemo(() => {
-        return products;
-    }, [products])
-
   return (
     <ul className={styles.ul}>
-        {memoizedProducts.length === 0 ? <Loading />: (
-            memoizedProducts.map(product => (
+        {products.length === 0 ? <Loading />: (
+            products.map(product => (
                 <Product product={product} />
             )) 
         )}
