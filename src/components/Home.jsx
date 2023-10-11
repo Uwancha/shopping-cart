f874cab4a5d2c1f6799bb9c6a52c835a57a7a891
Uwancha <sito2alemu@gmail.com>
2023-10-11 21:14:31 +0300

    feat: Import and render ImageSlider component insid Hompe page Use ImageSlidder instead of image to attract users

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,26 @@
 import React from "react";
-import cartIcon from "../assets/carts_891462.png";
 import styles from "../styles/home.module.css";
+import ImageSlider from "./ImageSlider";
 
 function Home() {
     return (
-        <div className={styles.home}>
-            <div className={styles.message}>
-                <div>
-                    <h2 className={styles.h2}>Welcome! Happy Shopping</h2>
-                    <p className={styles.p}>You know you can buy anything here! Well, actually No! This is a Demo!</p>
-                    <p className={styles.p}> But you know what, You don't know what you don't know</p>
+        <div className={styles.container}>
+            <div className={styles.home}>
+                <div className={styles.message}>
+                    <div>
+                        <h2 className={styles.h2}>Welcome! Happy Shopping</h2>
+                        <p className={styles.p}>You know you can buy anything here! Well, actually No! This is a Demo!</p>
+                        <p className={styles.p}> But you know what, You don't know what you don't know</p>
+                    </div>
+                    <div className={styles.callToaction}>
+                        <p className={styles.ctaMessage }>50$ credit when you first register</p>
+                        <button className={styles.button}>Sign Up</button>
+                    </div>
                 </div>
-                <div className={styles.callToaction}>
-                    <p className={styles.ctaMessage }>50$ credit when you first register</p>
-                    <button className={styles.button}>Sign Up</button>
-                </div>
+                
             </div>
-            <div className={styles.imageContainer}>
-                <img src={cartIcon} className={styles.img} alt="shopping cart. Icon by Pixel Perfect." />
+            <div className={styles.slider}>
+                <ImageSlider  />
             </div>
         </div>
     )
