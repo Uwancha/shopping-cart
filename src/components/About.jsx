26a1eeedad2e4fb27cb925a59daf3924646c8fb6
Uwancha <sito2alemu@gmail.com>
2023-10-13 21:09:14 +0300

    Create About page Add About.jsx in components Define About component

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function About() {
+    
+    return (
+        <div>
+            <div>
+                <h3>Company</h3>
+            </div>
+            <div>
+                <h3>Help</h3>
+            </div>
+            <div>
+                <h3>Loacation</h3>
+            </div>
+        </div>
+    )
+}
+
+export default About;
